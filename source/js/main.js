2a9d50f4d9aab949590fd479d89e4e10ee2bcf67
Mikhail Tereshin <medvedstwo@gmail.com>
2025-04-12 23:03:37 +0300

    feat: добавляет разметку hero

--- source/js/main.js
+++ source/js/main.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
   initHeader();
   initTitleData();
 });
+
+
+import Swiper from 'swiper';
+import {Navigation, Pagination} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.hero-swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
