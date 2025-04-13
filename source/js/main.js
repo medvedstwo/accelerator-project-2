0881dc6761d5bc04e808c651f84a98f8f0f2b70b
Mikhail Tereshin <medvedstwo@gmail.com>
2025-04-13 13:03:27 +0300

    feat: добавляет фото в свайццпер в hero

--- source/js/main.js
+++ source/js/main.js
@@ -13,16 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 import Swiper from 'swiper';
-import {Navigation, Pagination} from "swiper/modules";
+import {Navigation, Pagination} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
 const swiper = new Swiper('.hero-swiper', {
-  // configure Swiper to use modules
   modules: [Navigation, Pagination],
   pagination: {
     el: '.swiper-pagination',
   },
-})
+});
