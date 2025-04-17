e9045be9d20a693006bb0b4fd59fae386517af01
Mikhail Tereshin <medvedstwo@gmail.com>
2025-04-17 22:27:47 +0300

    feat: добавляет стилизацию tours tablet and desktop

--- source/js/tours-swiper.js
+++ source/js/tours-swiper.js
@@ -5,6 +5,21 @@ const toursSwiper = new Swiper('.tours-swiper', {
   modules: [ Navigation],
   init: false,
   loop: false,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  },
+  navigation: {
+    nextEl: '.tours-swiper__button--next',
+    prevEl: '.tours-swiper__button--prev',
+    disabledClass: 'tours-swiper__button--disabled',
+  }
 });
 
 const initToursSwiper = () => {
