5c40bb1d057e378cc5da7e72e338cfcea73003f7
Mikhail Tereshin <medvedstwo@gmail.com>
2025-04-27 14:11:32 +0300

    feat: добавляет стилизацию gallery desktop

--- source/js/main.js
+++ source/js/main.js
@@ -22,17 +22,59 @@ document.addEventListener('DOMContentLoaded', () => {
   initReviewsSwiper();
 });
 
-const gallerySwiper = new Swiper('.gallery-swiper', {
-  modules: [ Navigation],
-  loop: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 4,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 5,
+// const gallerySwiper = new Swiper('.gallery-swiper', {
+//   modules: [ Navigation],
+//   loop: true,
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 4,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 5,
+//     }
+//   },
+// });
+
+const initGallerySwiper = (breakpoint, swiperClass, swiperSettings) => {
+  let swiper;
+
+  breakpoint = window.matchMedia(breakpoint);
+
+  const enableSwiper = function(className, settings) {
+    swiper = new Swiper(className, settings);
+  };
+
+  const checker = function() {
+    if (breakpoint.matches) {
+      return enableSwiper(swiperClass, swiperSettings);
+    } else {
+      if (swiper !== undefined) {
+        swiper.destroy(true, true);
+      }
     }
-  },
-});
+  };
+
+  breakpoint.addEventListener('change', checker);
+  checker();
+};
+
+initGallerySwiper(
+  '(max-width: 1339px)',
+  '.gallery-swiper',
+  {
+    modules: [ Navigation],
+    loop: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 4,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      }
+    },
+  }
+);
