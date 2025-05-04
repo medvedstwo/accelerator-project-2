14fedd83a780d98120920400e168f211d04c48cd
Mikhail Tereshin <medvedstwo@gmail.com>
2025-05-04 20:15:40 +0300

    feat: добавляетфавиконки

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
-
 import initHeader from './header.js';
 import initTitleData from './title-data.js';
 import initHeroSwiper from './hero-swiper.js';
